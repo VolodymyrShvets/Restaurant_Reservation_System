12ce493062f2715f0957a96036956a0c3d302eb6
Volodymyr Shvets <vshvets295@gmail.com>
2023-04-27 22:01:36 +0300

    Added main class

---
+++ RestaurantBookingSystem/src/main/java/com/restaurant/system/RestaurantBookingSystemApplication.java
@@ -0,0 +1,11 @@
+package com.restaurant.system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantBookingSystemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantBookingSystemApplication.class, args);
+    }
+}
