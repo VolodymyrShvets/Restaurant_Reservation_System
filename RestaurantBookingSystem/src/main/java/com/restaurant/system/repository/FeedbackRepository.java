85904e7b12ffe14e2dbf1ce68661b77614423d6f
Volodymyr Shvets <vshvets295@gmail.com>
2023-05-04 13:14:49 +0300

    Added new method to repository

--- RestaurantBookingSystem/src/main/java/com/restaurant/system/repository/FeedbackRepository.java
+++ RestaurantBookingSystem/src/main/java/com/restaurant/system/repository/FeedbackRepository.java
@@ -4,7 +4,9 @@ import com.restaurant.system.model.Feedback;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
-
+    List<Feedback> findAllByCustomerID(long customerID);
 }
