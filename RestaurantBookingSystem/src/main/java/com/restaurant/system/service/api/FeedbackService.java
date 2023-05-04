252c3e11832087e3336355a3c95f14eb4377de09
Volodymyr Shvets <vshvets295@gmail.com>
2023-05-04 13:15:18 +0300

    Added new method

--- RestaurantBookingSystem/src/main/java/com/restaurant/system/service/api/FeedbackService.java
+++ RestaurantBookingSystem/src/main/java/com/restaurant/system/service/api/FeedbackService.java
@@ -2,10 +2,14 @@ package com.restaurant.system.service.api;
 
 import com.restaurant.system.model.Feedback;
 
+import java.util.List;
+
 public interface FeedbackService {
     Feedback leaveFeedback(Feedback feedback);
 
     Feedback getFeedback(long id);
+
+    List<Feedback> getAllUserFeedbacks(long customerId);
 
     Feedback updateFeedback(Feedback feedback);
 
